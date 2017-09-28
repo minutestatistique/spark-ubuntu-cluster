84c4cdd4ab6f3c261295e922a2bc6cbc81f57fc9
minutestatistique <data.science.fr@gmail.com>
2017-09-28 20:03:30 +0200

    deleted java8 installation

--- resources/big-data/spark/java-8-install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install software-properties-common python-software-properties
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get install openjdk-8-jre
-sudo update-alternatives --config java
